3573bf09af2210cb04c5b9e0a7c71c497d636c3b
Ochoagram <ricardogochoa@me.com>
2022-08-03 17:42:29 -0500

    footers whats

--- components/ui/WhatsappBtn.tsx
+++ components/ui/WhatsappBtn.tsx
@@ -5,7 +5,7 @@ import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 export const WhatsappBtn = () => {
   return (
     <Link href='https://wa.me/5219982396146' target='_blank'>
-        <WhatsAppIcon color='info' fontSize="large" className="btnwhats" />
+        <WhatsAppIcon color='info' sx={{ width: "2em", height: "2em" }} className="btnwhats" />
     </Link>
   )
 }
