e56d7ab9fb9f3b09a0b4a0694909b2be3cb06a6f
Ochoagram <ricardogochoa@me.com>
2022-08-05 12:42:30 -0500

    change image functonality

--- components/redes/ShowRedes.tsx
+++ components/redes/ShowRedes.tsx
@@ -23,16 +23,16 @@ export const ShowRedes = () => {
         <Typography color='#fff'>Recibe las mejores noticias antes que todos.</Typography>
     </Box>
     <Box marginTop={ 1 }>
-        <Button sx={{ background:'linear-gradient(180deg, #D40D84 0%, #51117E 100%)', paddingLeft:'3rem', paddingRight:'3rem' }}><Typography fontWeight={ 700 } fontSize='15px' lineHeight='36px'>Cotizar show</Typography></Button>
+        <Button sx={{ background:'linear-gradient(180deg, #D40D84 0%, #51117E 100%)', paddingLeft:'3rem', paddingRight:'3rem', ":hover":{transform: 'scale(1.1)'} }}><Typography fontWeight={ 700 } fontSize='15px' lineHeight='36px'>Cotizar show</Typography></Button>
     </Box>
 
 
     <Box textAlign='center' margin={ 10 }>
         <Typography variant='h4' component='h3' fontWeight={700} color='#fff' textAlign='center'>Síguenos</Typography>
         <Box display='flex' alignItems='space-betwwen' justifyContent='center'>
-            <Link href="https://www.facebook.com/ShowsInfantilesEnCancun" target="_blank" margin='1rem'><FacebookIcon fontSize='large' color='info' /> </Link>
-            <Link href="https://www.youtube.com/channel/UC9NEPshZGCKB21t-V0TAgxA" target="_blank" margin='1rem'><YouTubeIcon fontSize='large' color='info' /></Link>
-            <Link href="https://www.instagram.com/esdrikidsoficial" target="_blank" margin='1rem'><InstagramIcon fontSize='large' color='info' /></Link>
+            <Link href="https://www.facebook.com/ShowsInfantilesEnCancun" target="_blank" margin='1rem'><FacebookIcon  className='socialicon'fontSize='large' color='info' /> </Link>
+            <Link href="https://www.youtube.com/channel/UC9NEPshZGCKB21t-V0TAgxA" target="_blank" margin='1rem'><YouTubeIcon className='socialicon' fontSize='large' color='info' /></Link>
+            <Link href="https://www.instagram.com/esdrikidsoficial" target="_blank" margin='1rem' ><InstagramIcon  className='socialicon'fontSize='large' color='info' /></Link>
         </Box>
     </Box>
 
--- components/ui/MainBanner.tsx
+++ components/ui/MainBanner.tsx
@@ -1,16 +1,39 @@
 import {useState, useEffect} from 'react'
 import { Box, Typography, Button } from '@mui/material'
 
+const imageArray = ['https://res.cloudinary.com/dfmfxdkx4/image/upload/v1659715889/web/pricesas_ho9bvk.jpg',
+'https://res.cloudinary.com/dfmfxdkx4/image/upload/v1659384131/web/dino_desktop_ygkqdf.jpg',
+'https://res.cloudinary.com/dfmfxdkx4/image/upload/v1659715889/web/PORTADAS-INICIO_uyb6ax.jpg',
+'https://res.cloudinary.com/dfmfxdkx4/image/upload/v1659715889/web/mickey_mrb6el.jpg',
+'https://res.cloudinary.com/dfmfxdkx4/image/upload/v1659715889/web/GAGA_hict91.jpg'];
+
+const imageArrayMobile = ['https://res.cloudinary.com/dfmfxdkx4/image/upload/v1659719808/web/princesas_mobile_jbprmb.jpg',
+'https://res.cloudinary.com/dfmfxdkx4/image/upload/v1659384709/web/dino_mobile_esvxx0.webp',
+'https://res.cloudinary.com/dfmfxdkx4/image/upload/v1659719807/web/pitbull_mobile_oyhfbq.jpg',
+'https://res.cloudinary.com/dfmfxdkx4/image/upload/v1659719807/web/mickey_mobile_oapz3e.jpg',
+'https://res.cloudinary.com/dfmfxdkx4/image/upload/v1659719807/web/gaga_mobile_ubr2ly.jpg'];
+
 export const MainBanner = () => {
 
     const [mobile, setMobile] = useState<boolean | undefined>(true)
 
+    const [count, setCount] = useState(0);
+    useEffect(() => {
+      const timerId = setInterval(() => {
+        
+        setCount(count => count + 1);
+      }, 4000);
+  
+      return () => clearInterval(timerId);
+    }, []);
+
+    const image = imageArray[count % imageArray.length];
+    const imageMobile = imageArrayMobile[count % imageArrayMobile.length];
+    
   useEffect(() => {
     const updateMobile = () => {
-
       setMobile(window.innerWidth < 900 ? true : false)
     }
-
     updateMobile()
     window.addEventListener('resize', updateMobile)
     return () => {
@@ -31,11 +54,11 @@ export const MainBanner = () => {
 
             <Box maxWidth='1920px' sx={{zIndex:'2', position:'absolute', padding:{ xs:'1rem', md:'2rem'}, marginTop:{ xs:'1rem', md:'0'} }} className='wrapper' >
               <Box color='#fff' maxWidth='650px' sx={{ textAlign:{xs:'center', md:'left'} }}>
-                  <Typography variant='h1' component={'h1'} fontWeight={ 800 } color='#9747FF' lineHeight='50px' > Mas de 100 show diferentes </Typography>
+                  <Typography variant='h1' component={'h1'} fontWeight={ 800 } color='#FAD531' lineHeight='50px' > Mas de 100 show diferentes </Typography>
                   <Typography variant='h5' component={'h4'} fontWeight={ 500 }  sx={{ lineHeight:{xs:'28px' ,md:'34px'} }} marginTop='1rem' >15 años organizando los mejores espectáculos del sureste mexicano. </Typography>
         
                   <Box marginTop={3}>
-                    <Button sx={{ background:'linear-gradient(180deg, #D40D84 0%, #51117E 100%)', paddingLeft:'2rem', paddingRight:'2rem' }}><Typography fontWeight={ 700 } fontSize='15px' lineHeight='36px'>Cotizar show</Typography></Button>
+                    <Button sx={{ background:'linear-gradient(180deg, #D40D84 0%, #51117E 100%)', paddingLeft:'2rem', paddingRight:'2rem',":hover":{transform: 'scale(1.1)'} }}><Typography fontWeight={ 700 } fontSize='15px' lineHeight='36px'>Cotizar show</Typography></Button>
                   </Box>
               </Box>
             </Box>
@@ -45,7 +68,7 @@ export const MainBanner = () => {
                 ?
                 <Box height='800px'>
                   <img
-                  src="https://res.cloudinary.com/dfmfxdkx4/image/upload/v1659384709/web/dino_mobile_esvxx0.webp"
+                  src={ imageMobile }
                   alt="Girl in a jacket"
                   style={{objectFit:"cover"}}
                   width='100%'
@@ -57,7 +80,7 @@ export const MainBanner = () => {
                 <Box>
                     <img
                 width='100%'
-                src="https://res.cloudinary.com/dfmfxdkx4/image/upload/v1659384131/web/dino_desktop_ygkqdf.jpg"
+                src={ image }
                 alt="Show infantil"
                 style={{boxSizing:"content-box"}}
                 />
